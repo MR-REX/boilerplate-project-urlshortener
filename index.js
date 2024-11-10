6eed21fb75c61f50a086f339f8520722524e23f6
Maksim Krylov <I-MR-REX@yandex.ru>
2024-11-10 14:52:37 +0000

    Completing a task

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+
+const URL = require('url').URL;
+
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,9 +23,63 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const urls = [];
+const urlPattern = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+
+function validateUrl(url) {
+  return new Promise((resolve, reject) => {
+    const urlObject = new URL(url);
+
+    if (!urlPattern.test(url)) {
+      reject(new Error('Invalid URL'));
+      return;
+    }
+
+    resolve({
+      exists: true,
+      url
+    });
+  });
+}
+
+function getShortLinkUrl(linkId) {
+  return (linkId && urls[linkId - 1]) || null;
+}
+
+function addShortLinkId(url) {
+  let linkId = urls.indexOf(url);
+
+  if (linkId === -1) {
+    urls.push(url);
+    linkId = urls.length;
+  }
+
+  return linkId;
+}
+
+app.post('/api/shorturl', function(req, res) {
+  validateUrl(req.body.url).then(result => {
+    if (!result.exists) {
+      throw new Error('URL doesn\'t exists');
+    }
+
+    res.json({
+      original_url: result.url,
+      short_url: addShortLinkId(result.url)
+    });
+  }).catch(error => res.json({ error: 'invalid url' }));
+});
+
+app.get('/api/shorturl/:shorturl_id', function(req, res) {
+  const linkId = +req.params.shorturl_id;
+  const url = getShortLinkUrl(linkId);
+
+  if (!url) {
+    res.json({ error: 'No short URL found for the given input' })
+    return;
+  }
+
+  res.redirect(url);
 });
 
 app.listen(port, function() {
